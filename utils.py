7020c953bd2133db0fea40d07a46cc7c135357e2
hoang1234vodoi <hoangle260302@gmail.com>
2024-05-15 05:37:07 +0700

    visualize data

--- utils.py
+++ utils.py
@@ -108,7 +108,7 @@ class Dataloader:
             y_train = np.asarray(y_train)
             y_test = np.asarray(y_test)
             # Filter only classes 0 and 1
-            l1 = 1
+            l1 = 3
             l2 = 7
             mask = (y_train == l1) | (y_train == l2)
             X_train, y_train = X_train[mask], y_train[mask]
